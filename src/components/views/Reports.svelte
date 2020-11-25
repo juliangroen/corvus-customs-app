e4d26e828457df26bfaf4c9d6283caf76aa4c31a
Julian <julian@jgroen.com>
2020-11-24 23:07:12 -0700

    completed all vehicles report

--- src/components/views/Reports.svelte
+++ src/components/views/Reports.svelte
@@ -3,18 +3,19 @@
     import { appData, parts, vehicles } from '../../stores';
     import Page from '../shared/Page.svelte';
     import Tile from '../shared/Tile.svelte';
+    import GenerateParts from './GenerateParts.svelte';
     const testVehicles = [
         VehicleFactory.createVehicle({
             year: '2020',
             make: 'Lorem',
             model: 'Ipsum',
             parts: {
-                tires: '1605932042704',
-                chargers: '1605934207764',
-                wheels: '1606187382788',
-                shocks: '1605935969491',
-                brakes: '1605935944162',
-                exhausts: '1604983672865',
+                tires: '1604983672865',
+                chargers: '1605932042700',
+                wheels: '1605934221884',
+                shocks: '1606187382788',
+                brakes: '1605935944370',
+                exhausts: '1605935952906',
             },
         }),
         VehicleFactory.createVehicle({
@@ -22,12 +23,12 @@
             make: 'Lorem',
             model: 'Ipsum',
             parts: {
-                tires: '1605932042704',
-                chargers: '1605934207764',
-                wheels: '1606187382788',
-                shocks: '1605935969491',
-                brakes: '1605935944162',
-                exhausts: '1604983672865',
+                tires: '1604983672865',
+                chargers: '1605932042700',
+                wheels: '1605934221884',
+                shocks: '1606187382788',
+                brakes: '1605935944370',
+                exhausts: '1605935952906',
             },
         }),
         VehicleFactory.createVehicle({
@@ -35,12 +36,12 @@
             make: 'Lorem',
             model: 'Ipsum',
             parts: {
-                tires: '1605932042704',
-                chargers: '1605934207764',
-                wheels: '1606187382788',
-                shocks: '1605935969491',
-                brakes: '1605935944162',
-                exhausts: '1604983672865',
+                tires: '1604983672865',
+                chargers: '1605932042700',
+                wheels: '1605934221884',
+                shocks: '1606187382788',
+                brakes: '1605935944370',
+                exhausts: '1605935952906',
             },
         }),
         VehicleFactory.createVehicle({
@@ -48,12 +49,12 @@
             make: 'Lorem',
             model: 'Ipsum',
             parts: {
-                tires: '1605932042704',
-                chargers: '1605934207764',
-                wheels: '1606187382788',
-                shocks: '1605935969491',
-                brakes: '1605935944162',
-                exhausts: '1604983672865',
+                tires: '1604983672865',
+                chargers: '1605932042700',
+                wheels: '1605934221884',
+                shocks: '1606187382788',
+                brakes: '1605935944370',
+                exhausts: '1605935952906',
             },
         }),
     ];
@@ -86,7 +87,7 @@
     };
 
     const currentDateTime = () => {
-        return new Date(`${new Date(new Date().getTime()).toString()} UTC`);
+        return new Date(new Date().getTime()).toString();
     };
 
     const lookupModel = (id) => {
@@ -120,11 +121,6 @@
                 on:click={() => handleSelect('allVehicles')}>
                 <span> View All Vehicles </span>
             </Tile>
-            <Tile
-                src="../assets/svg/cubes.svg"
-                on:click={() => handleSelect('allParts')}>
-                <span> View All Parts </span>
-            </Tile>
         </section>
     {:else if currentReport === 'allVehicles'}
         <h1 class="text-xl italic font-bold text-center mb-2">
@@ -133,9 +129,6 @@
         <h2 class="text-sm text-center mb-4">Generated: {currentDateTime()}</h2>
         <section class="flex space-x-1 text-sm max-w-xl overflow-x-auto">
             <div class="flex flex-col space-y-1 w-24">
-                <!-- <div class="bg-gray-600 rounded-lg text-white text-center p-2">
-                    <span class="opacity-0">SPACER</span>
-                </div> -->
                 {#each rowHeaders as item}
                     <div
                         class="bg-gray-600 rounded-lg text-white text-center h-8 px-1">
@@ -144,54 +137,47 @@
                 {/each}
             </div>
 
-            {#each testVehicles as vehicle}
+            {#each $vehicles as vehicle}
                 <div class="flex flex-col space-y-1 w-32">
-                    {#each Object.entries(vehicle.dbObject()) as [key, value] (key)}
-                        {#if key === 'parts'}
-                            {#each Object.entries(value) as [pKey, pValue] (pKey)}
-                                <div
-                                    class="bg-white rounded-lg text-xs text-center h-8 px-1">
-                                    <span
-                                        class="leading-8">{lookupModel(pValue)}</span>
-                                </div>
-                            {/each}
-                        {:else if key === 'id'}
-                            <div
-                                class="bg-white rounded-lg text-xs text-center h-8 px-1">
-                                <span class="leading-8">{value}</span>
-                            </div>
-                        {:else}
-                            <div
-                                class="bg-white rounded-lg text-center h-8 px-1">
-                                <span class="leading-8">{value}</span>
-                            </div>
-                        {/if}
-                    {/each}
+                    <div
+                        class="bg-white rounded-lg text-xs text-center h-8 px-1">
+                        <span class="leading-8">{vehicle.getId()}</span>
+                    </div>
+                    <div class="bg-white rounded-lg text-center h-8 px-1">
+                        <span class="leading-8">{vehicle.getYear()}</span>
+                    </div>
+                    <div class="bg-white rounded-lg text-center h-8 px-1">
+                        <span class="leading-8">{vehicle.getMake()}</span>
+                    </div>
+                    <div class="bg-white rounded-lg text-center h-8 px-1">
+                        <span class="leading-8">{vehicle.getModel()}</span>
+                    </div>
+                    <div class="bg-white rounded-lg text-center h-8 px-1">
+                        <span
+                            class="leading-8">{lookupModel(vehicle.getParts().tires)}</span>
+                    </div>
+                    <div class="bg-white rounded-lg text-center h-8 px-1">
+                        <span
+                            class="leading-8">{lookupModel(vehicle.getParts().chargers)}</span>
+                    </div>
+                    <div class="bg-white rounded-lg text-center h-8 px-1">
+                        <span
+                            class="leading-8">{lookupModel(vehicle.getParts().wheels)}</span>
+                    </div>
+                    <div class="bg-white rounded-lg text-center h-8 px-1">
+                        <span
+                            class="leading-8">{lookupModel(vehicle.getParts().shocks)}</span>
+                    </div>
+                    <div class="bg-white rounded-lg text-center h-8 px-1">
+                        <span
+                            class="leading-8">{lookupModel(vehicle.getParts().brakes)}</span>
+                    </div>
+                    <div class="bg-white rounded-lg text-center h-8 px-1">
+                        <span
+                            class="leading-8">{lookupModel(vehicle.getParts().exhausts)}</span>
+                    </div>
                 </div>
             {/each}
         </section>
-    {:else if currentReport === 'allParts'}
-        <section class="grid grid-cols-1 gap-2 mb-2">
-            <div>All Parts</div>
-        </section>
     {/if}
 </Page>
-<!-- <div
-                    class="col-span-1 bg-gray-600 rounded-lg text-white text-center p-2">
-                    <img
-                        class=""
-                        src="../assets/svg/cubes.svg"
-                        alt="Part Icon" />
-                </div> -->
-<!-- <div class="grid grid-cols-1 gap-4">
-    <div>Vehicles:</div>
-    {#each $vehicles as item}
-        <div>{item.getModel()}</div>
-    {/each}
-
-    <div>Parts:</div>
-
-    {#each $parts as item}
-        <div>{item.getName()}</div>
-    {/each}
-</div> -->
