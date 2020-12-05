293f2594435d6080b94dcf9e76f8bd7c2c8ed1e8
Julian <julian@jgroen.com>
2020-12-04 17:23:53 -0700

    added caching to parts store

--- src/stores.js
+++ src/stores.js
@@ -2,6 +2,7 @@ import { readable, writable } from 'svelte/store';
 import { db } from './firebase';
 import VehicleFactory from './models/VehicleFactory';
 import PartFactory from './models/PartFactory';
+import makePart from './shared/partHelper';
 
 export const appData = writable({
     admin: false,
@@ -9,6 +10,7 @@ export const appData = writable({
     loading: true,
     part: null,
     partEdit: false,
+    partsUpdated: false,
     selectedPart: false,
     user: null,
     view: 'Vehicles',
@@ -86,41 +88,61 @@ export const vehicles = (() => {
 })();
 
 export const parts = (() => {
-    const parts = readable([], (set) => {
-        const unsubscribe = db
-            .collection('parts')
-            .orderBy('id', 'asc')
-            .onSnapshot(
-                (snapshot) => {
-                    let data = snapshot.docs
-                        .map((val) => {
-                            let doc = val.data();
-                            switch (doc.category) {
-                                case 'tires':
-                                    return PartFactory.createTire(doc);
-                                case 'chargers':
-                                    return PartFactory.createCharger(doc);
-                                case 'wheels':
-                                    return PartFactory.createWheel(doc);
-                                case 'shocks':
-                                    return PartFactory.createShock(doc);
-                                case 'brakes':
-                                    return PartFactory.createBrake(doc);
-                                case 'exhausts':
-                                    return PartFactory.createExhaust(doc);
-                                default:
-                                    break;
-                            }
-                        })
-                        .filter((val) => val !== undefined);
-                    set(data);
-                },
-                (e) => {
-                    console.log(e);
-                }
-            );
+    const partsRef = db.collection('parts');
+    const partsCache = JSON.parse(localStorage.getItem('partsCache'));
+    const partsList = partsCache === null ? null : partsCache.map((item) => makePart(item.category, item));
+    console.log(partsCache);
+    let appDataStore = null;
+    const unsubAppData = appData.subscribe((value) => (appDataStore = value));
+    partsCache === null ? (appDataStore.partsUpdated = true) : null;
+    console.log(appDataStore.partsUpdated);
+    const parts = readable(partsList !== null ? partsList : [], async (set) => {
+        if (appDataStore.partsUpdated) {
+            await partsRef.get().then((res) => {
+                const snapshot = res.docs.map((doc) => doc.data());
+                set(snapshot.map((item) => makePart(item.category, item)));
+                localStorage.setItem('partsCache', JSON.stringify(snapshot));
+                console.log('pulled parts from database');
+                appDataStore.partsUpdated = false;
+            });
+        } else {
+            console.log('pulled parts from cache');
+        }
+
+        // const unsubscribe = db
+        //     .collection('parts')
+        //     .orderBy('id', 'asc')
+        //     .onSnapshot(
+        //         (snapshot) => {
+        //             let data = snapshot.docs
+        //                 .map((val) => {
+        //                     let doc = val.data();
+        //                     switch (doc.category) {
+        //                         case 'tires':
+        //                             return PartFactory.createTire(doc);
+        //                         case 'chargers':
+        //                             return PartFactory.createCharger(doc);
+        //                         case 'wheels':
+        //                             return PartFactory.createWheel(doc);
+        //                         case 'shocks':
+        //                             return PartFactory.createShock(doc);
+        //                         case 'brakes':
+        //                             return PartFactory.createBrake(doc);
+        //                         case 'exhausts':
+        //                             return PartFactory.createExhaust(doc);
+        //                         default:
+        //                             break;
+        //                     }
+        //                 })
+        //                 .filter((val) => val !== undefined);
+        //             set(data);
+        //         },
+        //         (e) => {
+        //             console.log(e);
+        //         }
+        //     );
         return () => {
-            unsubscribe();
+            unsubAppData();
         };
     });
     return parts;
